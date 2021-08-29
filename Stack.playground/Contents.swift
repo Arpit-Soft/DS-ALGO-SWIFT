b30739804980b51d863287092d6b31a7537dbf15
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-29 13:59:33 +0530

    Added Stack push and pop operation

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -1,2 +1,34 @@
 import UIKit
 
+struct Stack<Element> {
+    
+    private var storage = [Element]()
+    init() {}
+    
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+}
+
+extension Stack: CustomStringConvertible {
+    var description: String {
+        let topDivider = "---------top----------"
+        let bottomDivider = "\n----------"
+        let stackElements = storage.map {"\($0)"}.reversed().joined(separator: "\n")
+        return topDivider + stackElements + bottomDivider
+    }
+}
+
+
+var stack = Stack<Int>()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+print("Before Popping")
+print(stack)
+print("After Popping")
+stack.pop()
+print(stack)
