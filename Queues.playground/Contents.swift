ee43f5a72431f59ddad147571c3a2ee675d42a5f
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-29 17:29:29 +0530

    Added queues dequeue and enqueue methods

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
+struct Queue<Element> {
+    
+    private var elements = [Element]()
+    
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    
+    var peek: Element? {
+        return elements.first
+    }
+    
+    mutating func enqueue(_ element: Element) {
+        elements.append(element)
+    }
+    
+    mutating func dequeue() -> Element? {
+        return isEmpty ? nil : elements.removeFirst()
+    }
+}
 
+extension Queue: CustomStringConvertible {
+    var description: String {
+        return String(describing: elements)
+    }
+}
+
+
+var queue = Queue<Int>()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+print(queue)
+
+queue.dequeue()
