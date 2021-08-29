ee1db7d1a8c19285416aa48fff14c03309972e5e
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-29 18:31:57 +0530

    Added a tree strcuture

--- Tree.playground/Contents.swift
+++ Tree.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
-var str = "Hello, playground"
+struct TreeNode <Type> {
+    var value: Type
+    var children = [TreeNode]()
+    
+    init(value: Type) {
+        self.value = value
+    }
+    
+    mutating func add(_ node: TreeNode) {
+        children.append(node)
+    }
+}
+
+extension TreeNode: CustomStringConvertible {
+    var description: String {
+        return String(describing: "\(value) -> \(children.map {$0.value})")
+    }
+}
+
+var beverages = TreeNode(value: "Beverages")
+let hot = TreeNode(value: "Hot Drink")
+let cold = TreeNode(value: "Cold Drink")
+
+beverages.add(hot)
+beverages.add(cold)
+
+print(beverages)
+
+/*
+ 
+       Beverages
+         /   \
+        /     \
+  Hot Drink  Cold Drink
+ 
+ */
